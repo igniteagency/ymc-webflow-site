bdce7cf2fab1354bfb74e58c00ff2987a20421f1
Sylvain Girard <sylvain97@gmail.com>
2026-04-10 11:28:27 +1000

    Update external link treatment

--- src/utils/external-link.ts
+++ src/utils/external-link.ts
@@ -1,7 +1,13 @@
 const handleExternalLinks = (): void => {
-  // Select all anchor elements with data-external attribute set to "yes", "true", or "1"
   const externalLinks = document.querySelectorAll<HTMLAnchorElement>(
-    'a[data-external="yes"], a[data-external="true"], a[data-external="1"]'
+    `
+      a[data-external="yes"],
+      a[data-external="true"],
+      a[data-external="1"],
+      [data-external="yes"] a,
+      [data-external="true"] a,
+      [data-external="1"] a
+    `
   );
 
   externalLinks.forEach((link) => {
